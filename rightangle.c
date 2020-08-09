7790cd69a2ff2f2c5d3afc1da8a2f22a9ed732c7
madhulathaammireddy <69135884+madhulathaammireddy@users.noreply.github.com>
2020-08-09 08:32:06 +0530

    developed by Madhu latha
    This code is used to display the right angled triangle by placing binary digits 01

---
+++ rightangle.c
@@ -0,0 +1,15 @@
+\\ write a c program to display the right angle triangle using "01"
+
+#include<studio.h>
+void main()
+{
+int i,j,rows;
+printf("input number of rows");
+scanf("%d",&rows);
+for(i=1;i<=rows;i++)
+{
+for(j=1;j<=1;j++)
+printf("01");
+printf("\n");
+}
+}
